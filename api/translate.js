102cdad5cad76a3e75043419e2fa8a928722ea84
Kevin Ennis <kennis84@gmail.com>
2021-02-22 10:19:06 -0500

    Guess Vercel doesn't do multipart form data natively

--- api/translate.js
+++ api/translate.js
@@ -1,8 +1,25 @@
-const pipeline = require('../lib/pipeline');
+const pipeline   = require('../lib/pipeline');
+const multiparty = require('multiparty');
 
 module.exports = async( req, res ) => {
-  const buffer = req.body.file;
-  const text   = await pipeline( buffer );
+  const form = new multiparty.Form;
 
-  res.json({ text });
+  form.parse( req, async( err, fields, files ) => {
+    function bail( ex ) {
+      res.writeHead( 500, { 'content-type': 'text/plain' } );
+      res.end( String( err ) );
+    }
+
+    if ( err ) {
+      return bail( err );
+    }
+
+    try {
+      const file = files.file;
+      const text = await pipeline( file );
+      res.json({ text });
+    } catch ( ex ) {
+      bail( ex );
+    }
+  });
 };
