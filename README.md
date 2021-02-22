6d27a026afaad9179d434a3f0b304fc0c23750e9
Kevin Ennis <kennis84@gmail.com>
2021-02-22 09:25:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+astro
+===
+
+Built a thing to help [a friend](https://www.instagram.com/100astroportraits).
+
+### TL;DR
+
+Accept PDF exports from [Astro Gold](https://www.astrogold.io/), spit out
+some text that can be thrown into Illustrator.
---
+++ index.js
@@ -0,0 +1,15 @@
+const fs        = require('fs');
+const promisify = require('util').promisify;
+const pipeline  = require('./lib/pipeline');
+const compose   = require('./lib/util/compose');
+
+const INPUT  = 'transit2.pdf' ;
+const OUTPUT = 'output.txt';
+
+const translate = compose(
+  promisify( fs.writeFile ).bind( fs, OUTPUT ),
+  pipeline,
+  promisify( fs.readFile ).bind( fs, INPUT )
+);
+
+translate().then( console.log ).catch( console.error );
